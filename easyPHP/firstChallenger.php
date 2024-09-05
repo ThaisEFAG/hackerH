27267a126de51418742c791d66e360facfc307a2
Thaís Gonçalves <thaisefag@gmail.com>
2024-09-05 19:38:27 -0300

    Update secondChallenger.php

--- easyPHP/firstChallenger.php
+++ easyPHP/firstChallenger.php
@@ -1,5 +1,59 @@
 <?php
 
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference($arr)
+{
+    $leftDiagonal = 0;
+    $rightDiagonal = 0;
+    $countMtz = count($arr);
+
+
+    foreach ($arr as $indexLine  => $line) {
+
+
+        $rightSum = $line[$countMtz - 1 - $indexLine];
+        $leftDiagonal += $rightSum;
+
+
+        $leftSum = $line[$indexLine];
+        $rightDiagonal += $leftSum;
+    }
+
+    $result = abs($leftDiagonal - $rightDiagonal);
+    return $result;
+}
+
+
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$n = intval(trim(fgets(STDIN)));
+
+$arr = array();
+
+for ($i = 0; $i < $n; $i++) {
+    $arr_temp = rtrim(fgets(STDIN));
+
+    $arr[] = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+}
+
+$result = diagonalDifference($arr);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
+
+
+###########################################################################################
+
+
 /*
  * Complete the 'plusMinus' function below.
  *
